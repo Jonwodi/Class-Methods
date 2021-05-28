f9119fe7ec3afe17b507e679f7b60baa66aa40a2
Jordan Onwodi <>
2021-05-28 21:08:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# class Baby:
+#     sex = 'male'
+
+#     def __init__(self, scores = []):
+#         self.scores = scores
+    
+#     def avg(self):
+#         return round(sum(self.scores) / len(self.scores))
+
+#     @staticmethod
+#     def notice():
+#         return "Exams next week!"
+    
+#     @classmethod
+#     def what_is_sex(birth):
+#         return f"I'm a {birth.sex}"
+
+# print(Baby.what_is_sex())
+# print(Baby.sex)
